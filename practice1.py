e95e061dc5c96b012cf69edcd24f52aa188fd0f1
Abhishek Yadav <yadavabhishek66429@gmail.com>
2026-07-21 18:42:34 +0530

    identity Card

---
+++ practice1.py
@@ -0,0 +1,41 @@
+name ="abhishek Yadav"
+rollNo=346443
+address="Azamgarh Utter Pradesh"
+print(name)
+print(rollNo)
+print(address)
+
+#WAP to swap two number
+a=45
+b=90
+temp=0
+temp=a
+a=b
+b=temp
+print("after swaping a=",a)
+print("after swaping b=",b)
+print("*"*40)
+x=79
+y=20
+print("Before swaping x=",x)
+print("Before swaping y=",y)
+#left,right=right,left
+x,y=y,x
+print("After swaping x=",x)
+print("After swaping y=",y)
+
+#WAP to print student detail for id card in diffrent lines
+print("#"*45)
+name=input("Enter Name of the student=")
+fatherName=input(" Father's Name= ")
+grade=input("Enter Grade =")
+rollNo=int(input("Enter roll Number of student="))
+email_id=input("Enter email=")
+ph_no=int(input("Enter PhoneNo ="))
+print("Studente identity card")
+print("Name=",name)
+print("Father's Name=",fatherName)
+print("Grade is=",grade)
+print("rollNo=",rollNo)
+print("Email Id=",email_id)
+print("Phone No=",ph_no)
